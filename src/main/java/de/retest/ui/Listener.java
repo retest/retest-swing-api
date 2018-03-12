97cddee13825d894121b08caa51c8cfbd75ec69c
Jeremias Roessler <jeremias.roessler@retest.de>
2018-03-12 20:17:25 +0100

    Rename method to match name of previous version

--- src/main/java/de/retest/ui/Listener.java
+++ src/main/java/de/retest/ui/Listener.java
@@ -6,5 +6,5 @@ public interface Listener {
 
 	// Needs to implement ctor with EventRecorder to send the recorded events to
 
-	void addListener(Component component);
+	void addListenerTo(Component component);
 }
